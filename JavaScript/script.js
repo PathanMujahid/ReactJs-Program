4cbe8932ed28d5dd34f66913877ccff945680423
Pathan Mujahid <105581805+PathanMujahid@users.noreply.github.com>
2023-03-02 14:00:57 +0530

    my files

---
+++ JavaScript/script.js
@@ -0,0 +1,31 @@
+ var name="Mujahid";
+ var age=26;
+ var weight=69.5;
+ var hight=5.7;
+ var reactcomletion=false;
+
+ console.log( name);
+ console.log(age);
+ console.log(weight);
+ console.log(hight);
+ console.log(react-comletion);
+
+ var travel_option=[
+    {
+    option:'car',
+    duration:'30min'
+ },
+{
+    option:'bus',
+    duration:'40min'
+},
+{
+    option:'bike',
+    duration:'20min'
+
+}]
+var maxduration='40min'
+var minduration='20min'
+var travel_option=minduration
+
+for(
